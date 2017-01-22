29a84aa42299277285e427ad07b71dcdefa3af76
davidkatz <15Dkatz@shcp.edu>
2017-01-21 16:37:34 -0800

    change to hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root 'application#goodbye'
+  root 'application#hello'
   #finds the application controller and calls the hello method just from the url parameter, woah.
 end
